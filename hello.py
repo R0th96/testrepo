3dac055b5e4331005509034885ebd2c5898028c6
R0th96 <htetwaiyantoe.mmr@gmail.com>
2024-05-07 21:36:25 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+#Say hello to user
+print("Hello Dear User")
+
+#Asking for name
+name = input("What is your name, Sir?")
+print("Then, Hello Dear ", name)
+
+#Say thanks you
+print("Thanks for using our service bitch", name)
+
